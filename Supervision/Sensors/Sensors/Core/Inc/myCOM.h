3fa16c8fd32f17794d02a2b21f584d574d92bbc8
Kristof0127 <kristof.olah9@t-online.hu>
2021-03-05 14:09:39 +0100

    DeleteStartup

--- Supervision/Sensors/Sensors/Core/Inc/myCOM.h
+++
@@ -1,7 +0,0 @@
-#include "stdint.h"
-#include "stm32f7xx_hal.h"
-#include "string.h"
-
-UART_HandleTypeDef huart3;
-
-void PCsend(char* sendData);
--- Supervision/Sensors/Sensors/Core/Src/myCOM.c
+++
@@ -1,6 +0,0 @@
-#include "myCOM.h"
-
-
-void PCsend(char* sendData){
-	HAL_UART_Transmit(&huart3, (uint8_t*)sendData, strlen(sendData), 15000);
-}
