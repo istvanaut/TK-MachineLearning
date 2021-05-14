40796c05c0a476c35436e235dc2443b5c43ce8a2
theorska <turoczyd6@gmail.com>
2021-05-14 12:38:30 +0200

    Added save to replay

--- Microcontroller/Network/ReinforcementlearningElements/ReplayMemory.py
+++ Microcontroller/Network/ReinforcementlearningElements/ReplayMemory.py
@@ -1,4 +1,5 @@
 import random
+import csv
 from collections import namedtuple
 
 Transition = namedtuple('Transition',
@@ -12,16 +13,29 @@ class ReplayMemory(object):
         self.capacity = capacity
         self.memory = []
         self.position = 0
+        self.time = []
 
-    def push(self, *args):
+    def push(self, *args, **kwargs):
         """Saves a transition."""
         if len(self.memory) < self.capacity:
             self.memory.append(None)
+            self.time.append(None)
         self.memory[self.position] = Transition(*args)
+        self.time[self.position] = kwargs.get('time')
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
+
+    def save(self):
+        f = dict(zip(self.time, self.memory))
+        w = csv.writer(open("output.csv", "w"))
+
+        top_row = ['Time', 'state_image', 'state_features', 'action', 'next_image', 'next_features', 'reward']
+        w.writerow(top_row)
+        for key, tuple in dict.items():
+            val = [_ for _ in tuple]
+            w.writerow([key]+val)
 
     def __len__(self):
         return len(self.memory)
