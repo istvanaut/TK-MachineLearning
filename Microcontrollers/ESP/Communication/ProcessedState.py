c821ac7854f260536250835d18ffcc8d1898e06e
ElxanAslanov <elxan.aslanov2000@gmail.com>
2021-12-10 03:23:19 +0100

    mistake comment deleted

--- Microcontrollers/ESP/Communication/ProcessedState.py
+++ Microcontrollers/ESP/Communication/ProcessedState.py
@@ -56,9 +56,9 @@ class ProcessedState:
             'ultra_sound_left': int.from_bytes(prev_state[4:8], byteorder='little', signed=False),
             'ultra_sound_right': int.from_bytes(prev_state[8:12], byteorder='little', signed=False),
             'laser': int.from_bytes(prev_state[12:16], byteorder='little', signed=False),
-            # 'left_motor': struct.unpack('f', prev_state[16:20]),
-            # 'right_motor': struct.unpack('f', prev_state[20:24]),
-            # 'reward': struct.unpack('f', prev_state[24:28]),
+            'left_motor': struct.unpack('f', prev_state[16:20]),
+            'right_motor': struct.unpack('f', prev_state[20:24]),
+            'reward': struct.unpack('f', prev_state[24:28]),
             'image': np.array(prev_image)
         }
 
