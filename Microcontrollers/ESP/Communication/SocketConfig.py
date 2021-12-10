1f995585c2031682bcdda90a71cde9f3993bf903
ElxanAslanov <elxan.aslanov2000@gmail.com>
2021-12-10 03:12:53 +0100

    Camera size and payload size for image changed to 9216

--- Microcontrollers/ESP/Communication/SocketConfig.py
+++ Microcontrollers/ESP/Communication/SocketConfig.py
@@ -5,7 +5,7 @@ class SocketConfig:
     def __init__(self):
         self.REQUEST_SIZE = 1  # Request size in byte
         self.PAYLOAD_SIZE = 44  # Payload size in byte
-        self.CAMERA_SIZE = 2048  # Camera pic size in byte
+        self.CAMERA_SIZE = 9216  # Camera pic size in byte
         self.TOTAL_SIZE = self.REQUEST_SIZE + self.PAYLOAD_SIZE + self.CAMERA_SIZE
 
         # Request types
@@ -33,7 +33,7 @@ class SocketConfig:
 
         self.WEIGHTS_CHUNKS = 1024  # size of weight chunks
 
-        self.payload_sizes = [28, 16, 1024, 1024]
+        self.payload_sizes = [28, 16, 9216, 9216]
 
         self.HOST = '192.168.131.153'  # Standard loopback interface address (localhost)
         self.PORT = 3333  # Port to listen on (non-privileged ports are > 1023)
